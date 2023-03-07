7cecdc99dd578a5106b3d9567e64bd4ec1109e6f
shaik Asif <smd.20sa@gmail.com>
2023-03-07 15:23:39 +0530

    Home and tables

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./components/Pages/Error";
 import Header from "./components/Header";
 import About from "./components/Pages/About";
+import RootLayout from "./components/Pages/root";
+import Home from "./components/Pages/Home";
 const router = createBrowserRouter([
-  { path: "/about", element: <About /> },
-  { path: "/", element: <Header /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/about", element: <About /> },
+      // { path: "/home", element: <Home /> },
+    ],
+  },
 ]);
 const App = () => {
   return <RouterProvider router={router} />;
---
+++ src/components/Pages/Error.jsx
@@ -0,0 +1,15 @@
+import Header from "../Header";
+
+function Error() {
+  return (
+    <>
+      <Header />
+      <div className="mt-5 w-100 font-weight-bolder d-flex align-items-center justify-content-center">
+        <h1 className="p-5 mt-5 display-1 font-weight-bolder">404 Error</h1>
+        <h2 className="display-4">Not Found!!!</h2>
+      </div>
+    </>
+  );
+}
+
+export default Error;
