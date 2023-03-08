35bea6f5596e251fcc9dfcf8ff958f1ecc52c7a9
shaik Asif <smd.20sa@gmail.com>
2023-03-08 16:27:35 +0530

    contactuspageandDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-
-// import Error from "./components/Pages/Error";
-
+import Error from "./components/Pages/Error";
 import About from "./components/Pages/About";
 import Home from "./components/Pages/Home";
 import Store from "./components/Pages/Store";
+import ContactUs from "./components/Pages/ContactUs";
 
 const App = () => {
   return (
@@ -16,6 +15,8 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/store" element={<Store />} />
           <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="*" element={<Error />} />
         </Routes>
       </main>
     </div>
