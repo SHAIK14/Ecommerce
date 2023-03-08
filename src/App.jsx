5450cb6842d9a7e24decbe5825e9dc4d4b7f32da
shaik Asif <smd.20sa@gmail.com>
2023-03-08 16:15:12 +0530

    RouteStyleUpdated

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Error from "./components/Pages/Error";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+
+// import Error from "./components/Pages/Error";
+
 import About from "./components/Pages/About";
-import RootLayout from "./components/Pages/root";
 import Home from "./components/Pages/Home";
 import Store from "./components/Pages/Store";
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    errorElement: <Error />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/about", element: <About /> },
-      { path: "/", element: <Header /> },
-      { path: "/store", element: <Store /> },
-    ],
-  },
-]);
+
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <div>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
